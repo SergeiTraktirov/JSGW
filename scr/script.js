eded92d5f244d485c904f405fda399e2c161b30f
SergeiTraktirov <119735924+SergeiTraktirov@users.noreply.github.com>
2022-12-05 12:51:08 +0400

    Add files via upload

--- scr/script.js
+++ scr/script.js
@@ -1,6 +1,21 @@
-let res = $('.res').html()
+let prt__count = $('.prt__count').html()
+//let cena = $('.cena').html()
+//let itog = $('.itog').html()
+
 function plus() {
-	res++
-	$('.res').html(res)
+	prt__count++
+	$('.prt__count').html(prt__count)
+//	$('.itog').html(cena * res)
 }
+
+function minus() {
+	if (prt__count > 1) {
+	prt__count--
+	$('.prt__count').html(prt__count)
+//	$('.itog').html(cena * res)
+	}
+}
+
+
 $('.button_plus').on('click', plus)
+$('.button_minus').on('click', minus)
